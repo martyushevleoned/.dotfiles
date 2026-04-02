632d8d6c3c399642889237a19cfc21b542aa1098
l.martiushev <l.martiushev@prosoftsystems.ru>
2026-04-02 11:36:20 +0500

    add nvim hop plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,7 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
     { -- Цветовая схема
         'navarasu/onedark.nvim',
-        priority = 1000,
         config = function()
             require('onedark').setup({ style = 'darker' }) -- style = 'dark' / 'darker' / 'cool' / 'deep' / 'warm' / 'warmer' / 'light'
             require('onedark').load()
@@ -34,30 +33,6 @@ require('lazy').setup({
             })
         end,
     },
-    { -- Буфферы
-        'akinsho/bufferline.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
-            require('bufferline').setup({ options = { numbers = 'buffer_id' } })
-        end,
-    },
-    { -- Поиск и навигация
-        'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            require('telescope').setup()
-        end,
-    },
-    { -- Bыделение синтаксиса
-        'nvim-treesitter/nvim-treesitter',
-    },
-    { -- Просмотр git diff
-        'sindrets/diffview.nvim',
-        dependencies = 'nvim-lua/plenary.nvim',
-    },
-    { -- Отметки изменения строк
-        'lewis6991/gitsigns.nvim',
-    },
     { -- Автодополнение
         'hrsh7th/nvim-cmp',
         dependencies = {
@@ -66,9 +41,8 @@ require('lazy').setup({
             'hrsh7th/cmp-path',
         },
         config = function()
-            local cmp = require('cmp')
-            cmp.setup({
-                sources = cmp.config.sources({
+            require('cmp').setup({
+                sources = require('cmp').config.sources({
                     { name = 'cmdline' }, 
                     { name = 'buffer' },
                     { name = 'path' },
@@ -76,10 +50,33 @@ require('lazy').setup({
             })
         end,
     },
+    { -- Поиск и навигация
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = true,
+    },
+    { -- Bыделение синтаксиса
+        'nvim-treesitter/nvim-treesitter',
+    },
+    { -- Буфферы
+        'akinsho/bufferline.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function() require('bufferline').setup({ options = { numbers = 'buffer_id' } }) end,
+    },
+    { -- Быстрый переход
+        'smoka7/hop.nvim',
+        config = true,
+    },
+    { -- Просмотр git diff
+        'sindrets/diffview.nvim',
+        dependencies = 'nvim-lua/plenary.nvim',
+    },
+    { -- Отметки изменения строк
+        'lewis6991/gitsigns.nvim',
+    },
     { -- Команды на русском
         'powerman/vim-plugin-ruscmd',
-        config = function()
-        end
+        config = function() end
     }
 })
 
@@ -88,6 +85,7 @@ vim.keymap.set('n', '<F1>', ':NvimTreeToggle<CR>', { noremap = true })
 vim.keymap.set('n', '<F2>', ':Telescope live_grep<CR>', { noremap = true })
 vim.keymap.set('n', '<F3>', ':Telescope find_files<CR>', { noremap = true })
 vim.keymap.set('n', '<F4>', ':DiffviewOpen HEAD~1<CR>', { noremap = true })
+vim.keymap.set({'n', 'v'}, '<F5>', function() require('hop').hint_words() end, { noremap = true })
 
 -- Настройки VIM
 vim.opt.number=true

078b764f7663b1f2c1d75769f14f2d31cb6f66ff
l.martiushev <l.martiushev@prosoftsystems.ru>
2026-07-13 18:34:47 +0500

    leader hotkeys

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,6 +31,7 @@ require('lazy').setup({
                 git = { ignore = false },
                 filters = { dotfiles = false },
             })
+            vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true })
         end,
     },
     { -- Автодополнение
@@ -53,7 +54,10 @@ require('lazy').setup({
     { -- Поиск и навигация
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        config = true,
+        config = function() 
+            vim.keymap.set('n', '<leader>g', ':Telescope live_grep<CR>', { noremap = true })
+            vim.keymap.set('n', '<leader>f', ':Telescope find_files<CR>', { noremap = true })
+        end,
     },
     { -- Bыделение синтаксиса
         'nvim-treesitter/nvim-treesitter',
@@ -66,32 +70,42 @@ require('lazy').setup({
     { -- Быстрый переход
         'smoka7/hop.nvim',
         config = true,
+        vim.keymap.set({'n', 'v'}, '<leader>h', function() require('hop').hint_words() end, { noremap = true })
     },
     { -- Просмотр git diff
         'sindrets/diffview.nvim',
         dependencies = 'nvim-lua/plenary.nvim',
+        config = function() 
+            vim.keymap.set('n', '<leader>do', ':DiffviewOpen HEAD~1<CR>', { noremap = true })
+            vim.keymap.set('n', '<leader>dc', ':DiffviewClose<CR>', { noremap = true })
+        end,
     },
     { -- Отметки изменения строк
         'lewis6991/gitsigns.nvim',
     },
     { -- Команды на русском
         'powerman/vim-plugin-ruscmd',
-        config = function() end
-    }
+        config = function() end,
+    },
+    { -- OpenCode
+        'nickjvandyke/opencode.nvim',
+        config = function() 
+            vim.g.opencode_opts = {}
+            vim.o.autoread = true
+            vim.keymap.set({ 'n', 'x' }, '<leader>oa', function() require('opencode').ask() end, { desc = 'Ask OpenCode…' })
+            vim.keymap.set({ 'n', 'x' }, '<leader>os', function() return require('opencode').operator('@this ') .. '_' end, { desc = 'Append selected to OpenCode', expr = true })
+        end,
+    },
 })
 
--- Бинды
+-- Utils
 vim.keymap.set({'n', 't'}, '<C-]>', '<C-\\><C-n>', { noremap = true })
-vim.keymap.set('n', '<F1>', ':NvimTreeToggle<CR>', { noremap = true })
-vim.keymap.set('n', '<F2>', ':Telescope live_grep<CR>', { noremap = true })
-vim.keymap.set('n', '<F3>', ':Telescope find_files<CR>', { noremap = true })
-vim.keymap.set('n', '<F4>', ':DiffviewOpen HEAD~1<CR>', { noremap = true })
-vim.keymap.set({'n', 't'}, '<F5>', '<C-\\><C-n>:set scrollback=1 <bar> set scrollback=10000<CR>i', { noremap = true })
-vim.keymap.set({'n', 'v'}, '<F6>', function() require('hop').hint_words() end, { noremap = true })
-vim.keymap.set('n', '<F7>', ':!git commit -a --amend --no-edit<CR>', { noremap = true })
-vim.keymap.set('t', '<F7>', 'git commit -a --amend --no-edit<CR>', { noremap = true })
-vim.keymap.set('n', '<F8>', ':!git add . && git commit -am \'\'<Left>', { noremap = true })
-vim.keymap.set('t', '<F8>', 'git add . && git commit -am \'\'<Left>', { noremap = true })
+vim.keymap.set({'n', 't'}, '<leader>l', '<C-\\><C-n>:set scrollback=1 <bar> set scrollback=10000<CR>i', { noremap = true })
+-- Git
+vim.keymap.set('n', '<leader>ga', ':!git commit -a --amend --no-edit<CR>', { noremap = true })
+vim.keymap.set('t', '<leader>ga', 'git commit -a --amend --no-edit<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>gс', ':!git add . && git commit -am \'\'<Left>', { noremap = true })
+vim.keymap.set('t', '<leader>gс', 'git add . && git commit -am \'\'<Left>', { noremap = true })
 
 -- Настройки VIM
 vim.opt.number=true
